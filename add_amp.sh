cb1f590d538ee60ef225b0c4a5a8a3ec41bcf95f
unammx <unammx>
2001-01-22 20:49:04 +0000

    2001-01-22  Arturo Espinosa  <arturo@ximian.com>
            * add_amp.sh: dumb program that fixes missing & when calling
              functions in perl code.
            * *-conf.in and be.pl.in: passed through add_amp.sh.

---
+++ add_amp.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+for i in `grep ^sub *-conf.in be.pl.in | cut -f2 -d' ' | sort | uniq`; do
+ for j in *-conf.in be.pl.in; do
+  echo $i:$j
+  sed 's/\([^_a-z]\)'$i'/\1\&'$i'/g;s/\&\+'$i'/\&'$i'/g;s/^sub \&\+'$i'/sub '$i'/g' $j > $j.1
+  mv $j.1 $j
+ done
+done
