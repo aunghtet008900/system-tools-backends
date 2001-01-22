7e13976f31e18e8af3fd71381b52c48e42a9ffe1
unammx <unammx>
2001-01-22 21:31:58 +0000

    Forgot this.

--- add_amp.sh
+++ add_amp.sh
@@ -3,7 +3,12 @@
 for i in `grep ^sub *-conf.in be.pl.in | cut -f2 -d' ' | sort | uniq`; do
  for j in *-conf.in be.pl.in; do
   echo $i:$j
-  sed 's/\([^_a-z]\)'$i'/\1\&'$i'/g;s/\&\+'$i'/\&'$i'/g;s/^sub \&\+'$i'/sub '$i'/g' $j > $j.1
+  sed '
+  s/\([^_a-z]\)'$i'/\1\&'$i'/g;
+  s/\&\+'$i'/\&'$i'/g;
+  s/^sub \&\+'$i'/sub '$i'/g;
+  s/\$\&/\$/g;s/\@\&/\@/g;s/\%\&/\%/g
+  ' $j > $j.1
   mv $j.1 $j
  done
 done
