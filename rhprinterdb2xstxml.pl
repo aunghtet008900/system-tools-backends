09e10ed0cb3c9f80fc836dfe1b3eb4dfb5456225
hansp <hansp>
2001-07-05 00:35:46 +0000

    Quote XML strings properly.

--- rhprinterdb2xstxml.pl
+++ rhprinterdb2xstxml.pl
@@ -20,6 +20,37 @@ sub xml_leave
   $indent_level -= 2;
 }
 
+sub xml_quote
+{
+  my $in = $_[0];
+  my $out = "";
+  my @xe;
+  my $joined = 0;
+  my @xml_entities = ( "&lt;", '<', "&gt;", '>', "&apos;", '\'', "&quot;", '"', "&amp;", '&' );
+
+  
+  my @clist = split (//, $in);
+  
+  while (@clist)
+  {
+    # Find character and join its entity equivalent.
+    # If none found, simply join the character.
+	
+    $joined = 0;		# Cumbersome.
+    
+    for (@xe = @xml_entities; @xe && !$joined; )
+    {
+      if ($xe [1] eq $clist [0]) { $out = join ('', $out, $xe [0]); $joined = 1; }
+      shift @xe; shift @xe;
+    }
+	
+    if (!$joined) { $out = join ('', $out, $clist [0]); }
+    shift @clist;
+  }
+  
+  return $out;
+}
+
 sub convert_about
 {
   &print_xml_line ("<comment>");
@@ -34,7 +65,7 @@ sub convert_about
     s/\\n\\n/\n/g;
     s/\\n/\n/g;
 
-    print $_ . "\n";
+    print &xml_quote ($_) . "\n";
   }
 
   &print_xml_line ("</comment>");
@@ -51,7 +82,7 @@ sub convert
 
     if (/^StartEntry: *(.*)/)
     {
-      &print_xml_line ("<printerdef id='$1'>");
+      &print_xml_line ("<printerdef id='" . &xml_quote ($1) . "'>");
       &xml_enter;
     }
     elsif (/^EndEntry/)
@@ -61,11 +92,11 @@ sub convert
     }
     elsif (/^GSDriver: *(.*)/)
     {
-      &print_xml_line ("<gsdriver name='$1'/>");
+      &print_xml_line ("<gsdriver name='" . &xml_quote ($1) . "'/>");
     }
     elsif (/^Description: *{ *(.*) *}/)
     {
-      &print_xml_line ("<description>$1</description>");
+      &print_xml_line ("<description>" . &xml_quote ($1) . "</description>");
     }
     elsif (/^About:/)
     {
@@ -73,11 +104,11 @@ sub convert
     }
     elsif (/^Resolu?tion: *\{ *([a-zA-Z0-9]+) *\} *\{ *([a-zA-Z0-9]+) *\}/)
     {
-      &print_xml_line ("<resolution x='$1' y='$2'/>");
+      &print_xml_line ("<resolution x='" . &xml_quote ($1) . "' y='" . &xml_quote ($2) . "'/>");
     }
     elsif (/^BitsPerPixel: *\{ *([^\} ]+) *\} *\{ *([^\}]+)\}/)
     {
-      &print_xml_line ("<mode id='$1'>$2</mode>");
+      &print_xml_line ("<mode id='" . &xml_quote ($1) . "'>" . &xml_quote ($2) . "</mode>");
     }
     else
     {
