11563ad6e47c69aa41b0061d061741d3951d2555
carlosg <carlosg>
2003-12-30 00:08:17 +0000

    2003-12-30  Carlos Garnacho Parro  <garnacho@tuxerver.net>
             * Makefile.am: changes for passing distcheck
    	 * backends/autogen.sh, configure.in: ditto
    	 * src/common/CommonMakefile: ditto

--- autogen.sh
+++ autogen.sh
@@ -44,6 +44,36 @@ fi
   }
 }
 
+CONFIGURE=$srcdir/configure.in
+
+if grep "^AM_[A-Z0-9_]\{1,\}_GETTEXT" "$CONFIGURE" >/dev/null; then
+ if grep "sed.*POTFILES" "$CONFIGURE" >/dev/null; then
+  GETTEXTIZE=""
+ else
+  if grep "^AM_GLIB_GNU_GETTEXT" "$CONFIGURE" >/dev/null; then
+    GETTEXTIZE="glib-gettextize"
+  else
+    GETTEXTIZE="gettextize"
+  fi
+
+  $GETTEXTIZE --version < /dev/null > /dev/null 2>&1
+  if test $? -ne 0; then
+    echo
+    echo "**Error**: You must have \`$GETTEXTIZE' installed" \
+         "to compile $PKG_NAME."
+    DIE=1
+  fi
+ fi
+fi
+(grep "^AC_PROG_INTLTOOL" "$CONFIGURE" >/dev/null) && {
+(intltoolize --version) < /dev/null > /dev/null 2>&1 || {
+ echo
+ echo "**Error**: You must have \`intltoolize' installed" \
+      "to compile $PKG_NAME."
+ DIE=1
+ }
+}
+
 #grep "^AM_GNU_GETTEXT" $srcdir/configure.in >/dev/null && {
 #  grep "sed.*POTFILES" $srcdir/configure.in >/dev/null || \
 #  (gettext --version) < /dev/null > /dev/null 2>&1 || {
@@ -106,86 +136,81 @@ for coin in `find $srcdir -name configure.in -print`
 do 
   dr=`dirname $coin`
   if test -f $dr/NO-AUTO-GEN; then
-    echo skipping $dr -- flagged as no auto-gen
+	 echo skipping $dr -- flagged as no auto-gen
   else
-    echo processing $dr
-    macrodirs=`sed -n -e 's,AM_ACLOCAL_INCLUDE(\(.*\)),\1,gp' < $coin`
-    ( cd $dr
-      macrosdir=`find . -name macros -print`
-      for i in $macrodirs; do
-	if test -f $i/gnome-gettext.m4; then
-	  DELETEFILES="$DELETEFILES $i/gnome-gettext.m4"
-	fi
-      done
+	 echo processing $dr
+	 macrodirs=`sed -n -e 's,AM_ACLOCAL_INCLUDE(\(.*\)),\1,gp' < $coin`
+	 ( cd $dr
+		macrosdir=`find . -name macros -print`
+		for i in $macrodirs; do
+		    if test -f $i/gnome-gettext.m4; then
+			   DELETEFILES="$DELETEFILES $i/gnome-gettext.m4"
+		    fi
+		done
 
-      echo "deletefiles is $DELETEFILES"
-      aclocalinclude="$ACLOCAL_FLAGS"
-      for k in $aclocalinclude; do
-  	if test -d $k; then
-	  if [ -f $k/gnome.m4 -a "$GNOME_INTERFACE_VERSION" = "1" ]; then
-	    rm -f $DELETEFILES
-	  fi
-        fi
-      done
-      for k in $macrodirs; do
-  	if test -d $k; then
-          aclocalinclude="$aclocalinclude -I $k"
-	  if [ -f $k/gnome.m4 -a "$GNOME_INTERFACE_VERSION" = "1" ]; then
-	    rm -f $DELETEFILES
-	  fi
-        fi
-      done
-      if grep "^AM_GNU_GETTEXT" configure.in >/dev/null; then
-	if grep "sed.*POTFILES" configure.in >/dev/null; then
-	  : do nothing -- we still have an old unmodified configure.in
-	else
-	  echo "Creating $dr/aclocal.m4 ..."
-	  test -r $dr/aclocal.m4 || touch $dr/aclocal.m4
-	  echo "Running gettextize...  Ignore non-fatal messages."
-	  echo "no" | gettextize --force --copy
-	  echo "Making $dr/aclocal.m4 writable ..."
-	  test -r $dr/aclocal.m4 && chmod u+w $dr/aclocal.m4
-        fi
-      fi
-      if grep "^AM_GNOME_GETTEXT" configure.in >/dev/null; then
-	echo "Creating $dr/aclocal.m4 ..."
-	test -r $dr/aclocal.m4 || touch $dr/aclocal.m4
-	echo "Running gettextize...  Ignore non-fatal messages."
-	echo "no" | gettextize --force --copy
-	echo "Making $dr/aclocal.m4 writable ..."
-	test -r $dr/aclocal.m4 && chmod u+w $dr/aclocal.m4
-      fi
-      if grep "^AM_PROG_XML_I18N_TOOLS" configure.in >/dev/null; then
-        echo "Running xml-i18n-toolize... Ignore non-fatal messages."
-	xml-i18n-toolize --copy --force --automake
-      fi
-      if grep "^AM_PROG_LIBTOOL" configure.in >/dev/null; then
-	if test -z "$NO_LIBTOOLIZE" ; then 
-	  echo "Running libtoolize..."
-	  libtoolize --force --copy
-	fi
-      fi
-      echo "Running aclocal $aclocalinclude ..."
-      aclocal $aclocalinclude || {
-	echo
-	echo "**Error**: aclocal failed. This may mean that you have not"
-	echo "installed all of the packages you need, or you may need to"
-	echo "set ACLOCAL_FLAGS to include \"-I \$prefix/share/aclocal\""
-	echo "for the prefix where you installed the packages whose"
-	echo "macros were not found"
-	exit 1
-      }
+		echo "deletefiles is $DELETEFILES"
+		aclocalinclude="$ACLOCAL_FLAGS"
+		for k in $aclocalinclude; do
+  		    if test -d $k; then
+			   if [ -f $k/gnome.m4 -a "$GNOME_INTERFACE_VERSION" = "1" ]; then
+				  rm -f $DELETEFILES
+			   fi
+		    fi
+		done
+		for k in $macrodirs; do
+  		    if test -d $k; then
+			   aclocalinclude="$aclocalinclude -I $k"
+			   if [ -f $k/gnome.m4 -a "$GNOME_INTERFACE_VERSION" = "1" ]; then
+				  rm -f $DELETEFILES
+			   fi
+		    fi
+		done
 
-      if grep "^AM_CONFIG_HEADER" configure.in >/dev/null; then
-	echo "Running autoheader..."
-	autoheader || { echo "**Error**: autoheader failed."; exit 1; }
-      fi
-      echo "Running automake --gnu $am_opt ..."
-      automake --add-missing --gnu $am_opt ||
-	{ echo "**Error**: automake failed."; exit 1; }
-      echo "Running autoconf ..."
-      autoconf || { echo "**Error**: autoconf failed."; exit 1; }
-    ) || exit 1
+		if test "$GETTEXTIZE"; then
+		    echo "Creating $dr/aclocal.m4 ..."
+		    test -r aclocal.m4 || touch aclocal.m4
+		    echo "Running $GETTEXTIZE...  Ignore non-fatal messages."
+		    echo "no" | $GETTEXTIZE --force --copy
+		    echo "Making aclocal.m4 writable ..."
+		    test -r aclocal.m4 && chmod u+w aclocal.m4
+		fi
+		
+		if grep "^AC_PROG_INTLTOOL" configure.in >/dev/null; then
+		    echo "Running intltoolize..."
+		    intltoolize --copy --force --automake
+		fi
+		
+		if grep "^AM_PROG_XML_I18N_TOOLS" configure.in >/dev/null; then
+		    echo "Running xml-i18n-toolize... Ignore non-fatal messages."
+		    xml-i18n-toolize --copy --force --automake
+		fi
+		if grep "^AM_PROG_LIBTOOL" configure.in >/dev/null; then
+		    if test -z "$NO_LIBTOOLIZE" ; then 
+			   echo "Running libtoolize..."
+			   libtoolize --force --copy
+		    fi
+		fi
+		echo "Running aclocal $aclocalinclude ..."
+		aclocal $aclocalinclude || {
+		    echo
+		    echo "**Error**: aclocal failed. This may mean that you have not"
+		    echo "installed all of the packages you need, or you may need to"
+		    echo "set ACLOCAL_FLAGS to include \"-I \$prefix/share/aclocal\""
+		    echo "for the prefix where you installed the packages whose"
+		    echo "macros were not found"
+		    exit 1
+		}
+
+		if grep "^AM_CONFIG_HEADER" configure.in >/dev/null; then
+		    echo "Running autoheader..."
+		    autoheader || { echo "**Error**: autoheader failed."; exit 1; }
+		fi
+		echo "Running automake --gnu $am_opt ..."
+		automake --add-missing --gnu $am_opt ||
+		{ echo "**Error**: automake failed."; exit 1; }
+		echo "Running autoconf ..."
+		autoconf || { echo "**Error**: autoconf failed."; exit 1; }
+	 ) || exit 1
   fi
 done
 
