1a931780afa36b74cf4b3e4b0511c61f4dd19aa8
carlosg <carlosg>
2005-04-11 21:30:54 +0000

    2005-04-11  Carlos Garnacho Parro  <carlosg@gnome.org>
            * ABOUT-NLS: Added
            * configure.in, autogen.sh: removed gnome-autogen dependencies

--- autogen.sh
+++ autogen.sh
@@ -1,225 +1,101 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-if [[ x$srcdir -eq x ]]
-then
-       srcdir=.
-else
-       srcdir=$srcdir
-fi
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+ORIGDIR=`pwd`
+cd $srcdir
+
+PROJECT=system-tools-backends
+TEST_TYPE=-f
+FILE=system-tools-backends.pc.in
 
 DIE=0
 
-if [ -n "$GNOME2_PATH" ]; then
-	ACLOCAL_FLAGS="-I $GNOME2_PATH/share/aclocal $ACLOCAL_FLAGS"
-	PATH="$GNOME2_PATH/bin:$PATH"
-	export PATH
-fi
-
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: You must have \`autoconf' installed to compile $PKG_NAME."
-  echo "Download the appropriate package for your distribution,"
-  echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-  DIE=1
+	echo
+	echo "You must have autoconf installed to compile $PROJECT."
+	echo "Download the appropriate package for your distribution,"
+	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+	DIE=1
 }
 
-(grep "^AM_PROG_XML_I18N_TOOLS" $srcdir/configure.in >/dev/null) && {
-  (xml-i18n-toolize --version) < /dev/null > /dev/null 2>&1 || {
-    echo 
-    echo "**Error**: You must have \`xml-i18n-toolize' installed to compile $PKG_NAME."
-    echo "Get ftp://ftp.gnome.org/pub/GNOME/stable/sources/xml-i18n-tools/xml-i18n-tools-0.6.tar.gz"
-    echo "(or a newer version if it is available)"
-    DIE=1
-  }
+AUTOMAKE=automake-1.7
+ACLOCAL=aclocal-1.7
+
+($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 || {
+        AUTOMAKE=automake
+        ACLOCAL=aclocal
 }
 
-(grep "^AM_PROG_LIBTOOL" $srcdir/configure.in >/dev/null) && {
-  (libtool --version) < /dev/null > /dev/null 2>&1 || {
-    echo
-    echo "**Error**: You must have \`libtool' installed to compile $PKG_NAME."
-    echo "Get ftp://ftp.gnu.org/pub/gnu/libtool-1.2d.tar.gz"
-    echo "(or a newer version if it is available)"
-    DIE=1
-  }
+($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have automake installed to compile $PROJECT."
+	echo "Get ftp://ftp.cygnus.com/pub/home/tromey/automake-1.2d.tar.gz"
+	echo "(or a newer version if it is available)"
+	DIE=1
 }
 
-CONFIGURE=$srcdir/configure.in
+(libtoolize --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have libtoolize installed to compile $PROJECT."
+	echo "Install the libtool package from ftp.gnu.org or a mirror."
+	DIE=1
+}
 
-if grep "^AM_[A-Z0-9_]\{1,\}_GETTEXT" "$CONFIGURE" >/dev/null; then
- if grep "sed.*POTFILES" "$CONFIGURE" >/dev/null; then
-  GETTEXTIZE=""
- else
-  if grep "^AM_GLIB_GNU_GETTEXT" "$CONFIGURE" >/dev/null; then
-    GETTEXTIZE="glib-gettextize"
-  else
-    GETTEXTIZE="gettextize"
-  fi
-
-  $GETTEXTIZE --version < /dev/null > /dev/null 2>&1
-  if test $? -ne 0; then
-    echo
-    echo "**Error**: You must have \`$GETTEXTIZE' installed" \
-         "to compile $PKG_NAME."
-    DIE=1
-  fi
- fi
-fi
-(grep "^AC_PROG_INTLTOOL" "$CONFIGURE" >/dev/null) && {
 (intltoolize --version) < /dev/null > /dev/null 2>&1 || {
- echo
- echo "**Error**: You must have \`intltoolize' installed" \
-      "to compile $PKG_NAME."
- DIE=1
- }
-}
-
-#grep "^AM_GNU_GETTEXT" $srcdir/configure.in >/dev/null && {
-#  grep "sed.*POTFILES" $srcdir/configure.in >/dev/null || \
-#  (gettext --version) < /dev/null > /dev/null 2>&1 || {
-#    echo
-#    echo "**Error**: You must have \`gettext' installed to compile $PKG_NAME."
-#    echo "Get ftp://alpha.gnu.org/gnu/gettext-0.10.35.tar.gz"
-#    echo "(or a newer version if it is available)"
-#    DIE=1
-#  }
-#}
-
-#grep "^AM_GNOME_GETTEXT" $srcdir/configure.in >/dev/null && {
-#  grep "sed.*POTFILES" $srcdir/configure.in >/dev/null || \
-#  (gettext --version) < /dev/null > /dev/null 2>&1 || {
-#    echo
-#    echo "**Error**: You must have \`gettext' installed to compile $PKG_NAME."
-#    echo "Get ftp://alpha.gnu.org/gnu/gettext-0.10.35.tar.gz"
-#    echo "(or a newer version if it is available)"
-#    DIE=1
-#  }
-#}
-
-(automake --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: You must have \`automake' installed to compile $PKG_NAME."
-  echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.3.tar.gz"
-  echo "(or a newer version if it is available)"
-  DIE=1
-  NO_AUTOMAKE=yes
-}
-
-
-# if no automake, don't bother testing for aclocal
-test -n "$NO_AUTOMAKE" || (aclocal --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: Missing \`aclocal'.  The version of \`automake'"
-  echo "installed doesn't appear recent enough."
-  echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.3.tar.gz"
-  echo "(or a newer version if it is available)"
-  DIE=1
+	echo
+	echo "**Error**: You must have \`intltoolize' installed to compile $PROJECT."
+	DIE=1
 }
 
 if test "$DIE" -eq 1; then
-  exit 1
+	exit 1
 fi
+
+test $TEST_TYPE $FILE || {
+	echo "You must run this script in the top-level $PROJECT directory"
+	exit 1
+}
 
 if test -z "$*"; then
-  echo "**Warning**: I am going to run \`configure' with no arguments."
-  echo "If you wish to pass any to it, please specify them on the"
-  echo \`$0\'" command line."
-  echo
+	echo "I am going to run ./configure with no arguments - if you wish "
+        echo "to pass any to it, please specify them on the $0 command line."
 fi
 
-case $CC in
-xlc )
-  am_opt=--include-deps;;
-esac
+libtoolize --copy --force
 
-for coin in `find $srcdir -name configure.in -print`
-do 
-  dr=`dirname $coin`
-  if test -f $dr/NO-AUTO-GEN; then
-	 echo skipping $dr -- flagged as no auto-gen
-  else
-	 echo processing $dr
-	 macrodirs=`sed -n -e 's,AM_ACLOCAL_INCLUDE(\(.*\)),\1,gp' < $coin`
-	 ( cd $dr
-		macrosdir=`find . -name macros -print`
-		for i in $macrodirs; do
-		    if test -f $i/gnome-gettext.m4; then
-			   DELETEFILES="$DELETEFILES $i/gnome-gettext.m4"
-		    fi
-		done
+intltoolize --copy --force --automake
 
-		echo "deletefiles is $DELETEFILES"
-		aclocalinclude="$ACLOCAL_FLAGS"
-		for k in $aclocalinclude; do
-  		    if test -d $k; then
-			   if [ -f $k/gnome.m4 -a "$GNOME_INTERFACE_VERSION" = "1" ]; then
-				  rm -f $DELETEFILES
-			   fi
-		    fi
-		done
-		for k in $macrodirs; do
-  		    if test -d $k; then
-			   aclocalinclude="$aclocalinclude -I $k"
-			   if [ -f $k/gnome.m4 -a "$GNOME_INTERFACE_VERSION" = "1" ]; then
-				  rm -f $DELETEFILES
-			   fi
-		    fi
-		done
+echo $ACLOCAL $ACLOCAL_FLAGS
+$ACLOCAL $ACLOCAL_FLAGS
 
-		if test "$GETTEXTIZE"; then
-		    echo "Creating $dr/aclocal.m4 ..."
-		    test -r aclocal.m4 || touch aclocal.m4
-		    echo "Running $GETTEXTIZE...  Ignore non-fatal messages."
-		    echo "no" | $GETTEXTIZE --force --copy
-		    echo "Making aclocal.m4 writable ..."
-		    test -r aclocal.m4 && chmod u+w aclocal.m4
-		fi
-		
-		if grep "^AC_PROG_INTLTOOL" configure.in >/dev/null; then
-		    echo "Running intltoolize..."
-		    intltoolize --copy --force --automake
-		fi
-		
-		if grep "^AM_PROG_XML_I18N_TOOLS" configure.in >/dev/null; then
-		    echo "Running xml-i18n-toolize... Ignore non-fatal messages."
-		    xml-i18n-toolize --copy --force --automake
-		fi
-		if grep "^AM_PROG_LIBTOOL" configure.in >/dev/null; then
-		    if test -z "$NO_LIBTOOLIZE" ; then 
-			   echo "Running libtoolize..."
-			   libtoolize --force --copy
-		    fi
-		fi
-		echo "Running aclocal $aclocalinclude ..."
-		aclocal $aclocalinclude || {
-		    echo
-		    echo "**Error**: aclocal failed. This may mean that you have not"
-		    echo "installed all of the packages you need, or you may need to"
-		    echo "set ACLOCAL_FLAGS to include \"-I \$prefix/share/aclocal\""
-		    echo "for the prefix where you installed the packages whose"
-		    echo "macros were not found"
-		    exit 1
-		}
+## optionally feature autoheader
+(autoheader --version)  < /dev/null > /dev/null 2>&1 && autoheader
 
-		if grep "^AM_CONFIG_HEADER" configure.in >/dev/null; then
-		    echo "Running autoheader..."
-		    autoheader || { echo "**Error**: autoheader failed."; exit 1; }
-		fi
-		echo "Running automake --gnu $am_opt ..."
-		automake --add-missing --gnu $am_opt ||
-		{ echo "**Error**: automake failed."; exit 1; }
-		echo "Running autoconf ..."
-		autoconf || { echo "**Error**: autoconf failed."; exit 1; }
-	 ) || exit 1
-  fi
+$AUTOMAKE -a $am_opt
+autoconf || echo "autoconf failed - version 2.5x is probably required"
+
+cd $ORIGDIR
+
+run_configure=true
+for arg in $*; do
+    case $arg in 
+        --no-configure)
+            run_configure=false
+            ;;
+        *)
+            ;;
+    esac
 done
 
-conf_flags="--enable-maintainer-mode --enable-compile-warnings" #--enable-iso-c
-
-if test x$NOCONFIGURE = x; then
-  echo Running $srcdir/configure $conf_flags "$@" ...
-  $srcdir/configure $conf_flags "$@" \
-  && echo Now type \`make\' to compile $PKG_NAME || exit 1
+if $run_configure; then
+    $srcdir/configure --enable-maintainer-mode "$@"
+    echo 
+    echo "Now type 'make' to compile $PROJECT."
 else
-  echo Skipping configure process.
+    echo
+    echo "Now run 'configure' and 'make' to compile $PROJECT."
 fi
+
