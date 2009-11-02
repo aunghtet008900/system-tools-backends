aunghtet008900/system-tools-backends

commit ee59351bfa31ee60779a20a79b1b49f308a777b0

Bugs 519273 & 346342 - Clean Utils::File::run() logic by using array of arguments
Using system() with a string instead of an array raises various issues when strings passed include special chars. Overall, it's claner and safer to use an array everywhere. This requires us to use our own execution code since system() does not accept setting STDOUT, STDERR, LC_ALL, and starting processes in the background when using an array. Note that run_backticks() still use the old logic, which is safe(r) because those commandlines are static. However, this commit also cleans get_cmd_path() by merging do_get_cmd_path() into it, and removing duplicate settings like $PATH.

Users/Users.pm
@@ -439,32 +439,32 @@ sub get
 sub del_user
 {
 	my ($user) = @_;
-  my ($command);
+  my (@command);
 	
   if ($Utils::Backend::tool{"system"} eq "FreeBSD")
   {
-    $command = "$cmd_pw userdel -n \'" . $$user[$LOGIN] . "\' ";
+    @command = ($cmd_pw, "userdel", "-n", $$user[$LOGIN]);
   }
   else
   {
     if ($cmd_deluser)
     {
-      $command = "$cmd_deluser '". $$user[$LOGIN] . "'";
+      @command = ($cmd_deluser, $$user[$LOGIN]);
     }
     else
     {
-      $command = "$cmd_userdel \'" . $$user[$LOGIN] . "\'";
+      @command = ($cmd_userdel, $$user[$LOGIN]);
     }
   }
 
-  &Utils::File::run ($command);
+  &Utils::File::run (@command);
 }
 
 sub change_user_chfn
 {
   my ($login, $old_comment, $comment) = @_;
   my ($fname, $office, $office_phone, $home_phone);
-  my ($command, $str);
+  my (@command, $str);
 
   return if !$login;
 
@@ -474,14 +474,15 @@ sub change_user_chfn
 
   if ($Utils::Backend::tool{"system"} eq "FreeBSD")
   {
-    $command = "$cmd_pw usermod -n " . $login . " -c \'" . $str . "\'";
+    @command = ($cmd_pw, "usermod", "-n", $login,
+                                    "-c", $str);
   }
   else
   {
-    $command = "$cmd_usermod -c \'" . $str . "\' " . $login;
+    @command = ($cmd_usermod, "-c", $str, $login);
   }
 
-  &Utils::File::run ($command);
+  &Utils::File::run (@command);
 }
 
 # modifies /etc/shadow directly, not good practice,
@@ -515,11 +516,11 @@ sub modify_shadow_password
 sub set_passwd
 {
   my ($login, $password) = @_;
-  my ($pwdpipe, $command);
+  my ($pwdpipe);
 
   if ($Utils::Backend::tool{"system"} eq "FreeBSD")
   {
-
+    my ($command);
     $command = "$cmd_pw usermod -H 0";
     $pwdpipe = &Utils::File::run_pipe_write ($command);
     print $pwdpipe $password;
@@ -531,10 +532,10 @@ sub set_passwd
   }
   else
   {
-    $command = "$cmd_usermod " .
-        " -p '" . $password . "' " . $login;
+    my (@command);
+    @command = ($cmd_usermod, "-p", $password, $login);
 
-    &Utils::File::run ($command);
+    &Utils::File::run (@command);
   }
 }
 
@@ -552,7 +553,7 @@ sub add_user
 
     # FreeBSD doesn't create the home directory
     $home = $$user[$HOME];
-    &Utils::File::run ("$tool_mkdir -p $home");
+    &Utils::File::run ($tool_mkdir, "-p", $home);
 
     $command = "$cmd_pw useradd " .
         " -n \'" . $$user[$LOGIN] . "\'" .
@@ -562,6 +563,13 @@ sub add_user
         " -s \'" . $$user[$SHELL] . "\'" .
         " -H 0"; # pw(8) reads password from STDIN
 
+#    @command = ($cmd_pw, "useradd", "-n", $$user[$LOGIN],
+#                                    "-u", $$user[$UID],
+#                                    "-d", $$user[$HOME],
+#                                    "-g", $$user[$GID],
+#                                    "-s", $$user[$SHELL],
+#                                    "-H", "0"); # pw(8) reads password from STDIN
+
     $pwdpipe = &Utils::File::run_pipe_write ($command);
     print $pwdpipe $$user[$PASSWD];
     &Utils::File::close_file ($pwdpipe);
@@ -570,23 +578,22 @@ sub add_user
   {
     $home_parents = $$user[$HOME];
     $home_parents =~ s/\/+[^\/]+\/*$//;
-    &Utils::File::run ("$tool_mkdir -p $home_parents");
+    &Utils::File::run ($tool_mkdir, "-p", $home_parents);
 
-    $command = "$cmd_useradd" .
-        " -d \'" . $$user[$HOME]  . "\'" .
-        " -g \'" . $$user[$GID]   . "\'" .
-        " -s \'" . $$user[$SHELL] . "\'" .
-        " -u \'" . $$user[$UID]   . "\'" .
-        " \'"    . $$user[$LOGIN] . "\'";
+    @command = ($cmd_useradd, "-d", $$user[$HOME],
+                              "-g", $$user[$GID],
+                              "-s", $$user[$SHELL],
+                              "-u", $$user[$UID],
+                                    $$user[$LOGIN]);
 
-    &Utils::File::run ($command);
+    &Utils::File::run (@command);
     &modify_shadow_password ($$user[$LOGIN], $$user[$PASSWD]);
   }
   else
   {
     $home_parents = $$user[$HOME];
     $home_parents =~ s/\/+[^\/]+\/*$//;
-    &Utils::File::run ("$tool_mkdir -p $home_parents");
+    &Utils::File::run ($tool_mkdir, "-p", $home_parents);
 
     if ($cmd_adduser &&
         $Utils::Backend::tool{"platform"} !~ /^slackware/ &&
@@ -596,34 +603,34 @@ sub add_user
     {
       # use adduser if available and valid (slackware one is b0rk)
       # set empty gecos fields and password, they will be filled out later
-      $command = "$cmd_adduser --gecos '' --disabled-password" .
-          " --home \'"  . $$user[$HOME]   . "\'" .
-          " --gid \'"   . $$user[$GID]    . "\'" .
-          " --shell \'" . $$user[$SHELL]  . "\'" .
-          " --uid \'"   . $$user[$UID]    . "\'" .
-          " \'"         . $$user[$LOGIN]  . "\'";
+      @command = ($cmd_adduser, "--gecos", "",
+                                "--disabled-password",
+                                "--home", $$user[$HOME],
+                                "--gid", $$user[$GID],
+                                "--shell", $$user[$SHELL],
+                                "--uid", $$user[$UID],
+                                         $$user[$LOGIN]);
 
-      &Utils::File::run ($command);
+      &Utils::File::run (@command);
 
       # password can't be set in non-interactive
       # mode with adduser, call usermod instead
-      $command = "$cmd_usermod " .
-          " -p '" . $$user[$PASSWD] . "' " . $$user[$LOGIN];
+      @command = ($cmd_usermod, "-p", $$user[$PASSWD], $$user[$LOGIN]);
 
-      &Utils::File::run ($command);
+      &Utils::File::run (@command);
     }
     else
     {
       # fallback to useradd
-      $command = "$cmd_useradd -m" .
-          " -d \'" . $$user[$HOME]   . "\'" .
-          " -g \'" . $$user[$GID]    . "\'" .
-          " -p \'" . $$user[$PASSWD] . "\'" .
-          " -s \'" . $$user[$SHELL]  . "\'" .
-          " -u \'" . $$user[$UID]    . "\'" .
-          " \'"    . $$user[$LOGIN]  . "\'";
+      @command = ($cmd_useradd, "-m",
+                                "-d", $$user[$HOME],
+                                "-g", $$user[$GID],
+                                "-p", $$user[$PASSWD],
+                                "-s", $$user[$SHELL],
+                                "-u", $$user[$UID],
+                                      $$user[$LOGIN]);
 
-      &Utils::File::run ($command);
+      &Utils::File::run (@command);
     }
   }
 
@@ -652,27 +659,24 @@ sub change_user
   }
   elsif ($Utils::Backend::tool{"system"} eq "SunOS")
   {
-    $command = "$cmd_usermod" .
-        " -d \'" . $$new_user[$HOME]   . "\'" .
-        " -g \'" . $$new_user[$GID]    . "\'" .
-        " -l \'" . $$new_user[$LOGIN]  . "\'" .
-        " -s \'" . $$new_user[$SHELL]  . "\'" .
-        " -u \'" . $$new_user[$UID]    . "\'" .
-        " \'" . $$old_user[$LOGIN] . "\'";
+    @command = ($cmd_usermod, "-d", $$new_user[$HOME],
+                              "-g", $$new_user[$GID],
+                              "-l", $$new_user[$LOGIN],
+                              "-s", $$new_user[$SHELL],
+                              "-u", $$new_user[$UID],
+                                    $$old_user[$LOGIN]);
 
-    &Utils::File::run ($command);
+    &Utils::File::run (@command);
     &modify_shadow_password ($$new_user[$LOGIN], $$new_user[$PASSWD]);
   }
   else
   {
-    $command = "$cmd_usermod" .
-        " -d \'" . $$new_user[$HOME]   . "\'" .
-        " -g \'" . $$new_user[$GID]    . "\'" .
-        " -l \'" . $$new_user[$LOGIN]  . "\'" .
-        " -p \'" . $$new_user[$PASSWD] . "\'" .
-        " -s \'" . $$new_user[$SHELL]  . "\'" .
-        " -u \'" . $$new_user[$UID]    . "\'" .
-        " \'" . $$old_user[$LOGIN] . "\'";
+    @command = ($cmd_usermod, "-d", $$new_user[$HOME],
+                              "-g", $$new_user[$GID],
+                              "-l", $$new_user[$LOGIN],
+                              "-s", $$new_user[$SHELL],
+                              "-u", $$new_user[$UID],
+                                    $$old_user[$LOGIN]);
 
     &Utils::File::run ($command);
   }

Init/Services.pm
@@ -232,7 +232,7 @@ sub run_sysv_initd_script
 
   if (-f "$initd_path/$service")
   {
-    if (&Utils::File::run ("$initd_path/$service $arg") == 0)
+    if (&Utils::File::run ("$initd_path/$service", $arg) == 0)
     {
       &Utils::Report::do_report ("service_sysv_op_success", $service, $arg);
       &Utils::Report::leave ();
@@ -589,15 +589,15 @@ sub run_bsd_script
   if (! -x $service)
   {
     $chmod = 1;
-    &Utils::File::run ("chmod ugo+x $service");
+    &Utils::File::run ("chmod", "ugo+x", $service);
   }
 
-  &Utils::File::run ("$service $arg", 1);
+  &Utils::File::run_bg ($service, $arg);
 
   # return it to it's normal state
   if ($chmod)
   {
-    &Utils::File::run ("chmod ugo-x $service");
+    &Utils::File::run ("chmod", "ugo-x", $service);
   }
 }
 
@@ -625,13 +625,13 @@ sub set_bsd_services
 
     if ($status == $SERVICE_START)
     {
-      &Utils::File::run ("chmod ugo+x $script");
+      &Utils::File::run ("chmod", "ugo+x", $script);
       &run_bsd_script ($script, "start");
     }
     else
     {
       &run_bsd_script ($script, "stop");
-      &Utils::File::run ("chmod ugo-x $script");
+      &Utils::File::run ("chmod", "ugo-x", $script);
     }
   }
 }
@@ -778,7 +778,7 @@ sub run_gentoo_script
 
   if (&gentoo_service_exists ($service))
   {
-    if (!&Utils::File::run ("/etc/init.d/$service $arg"))
+    if (!&Utils::File::run ("/etc/init.d/$service", $arg))
     {
       &Utils::Report::do_report ("service_sysv_op_success", $service, $arg);
       &Utils::Report::leave ();
@@ -804,13 +804,13 @@ sub set_gentoo_service_status
 
   if ($status == $SERVICE_START)
   {
-    &Utils::File::run ("rc-update add $script $rl");
+    &Utils::File::run ("rc-update", "add", $script, $rl);
     &run_gentoo_script ($script, "start");
   }
   else
   {
     &run_gentoo_script ($script, "stop");
-    &Utils::File::run ("rc-update del $script $rl");
+    &Utils::File::run ("rc-update", "del", $script, $rl);
   }
 }
 
@@ -909,7 +909,7 @@ sub run_rcng_script
 
   &Utils::Report::enter ();
 
-  if (!&Utils::File::run ("/etc/rc.d/$service $arg"))
+  if (!&Utils::File::run ("/etc/rc.d/$service", $arg))
   {
     &Utils::Report::do_report ("service_sysv_op_success", $service, $arg);
     &Utils::Report::leave ();
@@ -1092,7 +1092,7 @@ sub set_suse_services
     $rllist = "";
     %configured_runlevels = {};
 
-    &Utils::File::run ("insserv -r $script");
+    &Utils::File::run ("insserv", "-r", $script);
 
     foreach $rl (@$runlevels)
     {
@@ -1110,7 +1110,7 @@ sub set_suse_services
     {
       $rllist =~ s/,$//;
 
-      &Utils::File::run ("insserv $script,start=$rllist");
+      &Utils::File::run ("insserv", $script, "start=$rllist");
     }
 
     if (!$configured_runlevels{$default_runlevel})
@@ -1146,7 +1146,7 @@ sub run_smf_svcadm
 
   if (&smf_service_exists ($service))
   {
-    if (!&Utils::File::run ("svcadm $op{$arg} $service"))
+    if (!&Utils::File::run ("svcadm", $op{$arg}, $service))
     {
       &Utils::Report::do_report ("service_sysv_op_success", $service, $arg);
       &Utils::Report::leave ();
@@ -1245,11 +1245,11 @@ sub set_smf_service_status
 
   if ($active == $SERVICE_START)
   {
-    &Utils::File::run ("svcadm enable -s $service");
+    &Utils::File::run ("svcadm", "enable", "-s", $service);
   }
   else
   {
-    &Utils::File::run ("svcadm disable -s $service");
+    &Utils::File::run ("svcadm", "disable", "-s", $service);
   }
 }
 

Network/Hosts.pm
@@ -29,7 +29,7 @@ sub run_hostname
 
   &Utils::Report::enter ();
   &Utils::Report::do_report ("network_hostname_set", $hostname);
-  &Utils::File::run ("hostname $hostname");
+  &Utils::File::run ("hostname", $hostname);
   &Utils::Report::leave ();
 }
 

Network/Ifaces.pm
@@ -126,8 +126,7 @@ sub get_linux_wireless_ifaces
   my ($fd, $line);
   my (@ifaces, $command);
 
-  $command = &Utils::File::get_cmd_path ("iwconfig");
-  open $fd, "$command |";
+  $fd = &Utils::File::run_pipe_read ("iwconfig");
   return @ifaces if $fd eq undef;
 
   while (<$fd>)

Shares/NFS.pm
@@ -374,8 +374,8 @@ sub set
 
   if ($Utils::Backend::tool{"system"} eq "SunOS")
   {
-    &Utils::File::run ("unshareall -F nfs");
-    &Utils::File::run ("shareall -F nfs");
+    &Utils::File::run ("unshareall", "-F", "nfs");
+    &Utils::File::run ("shareall", "-F", "nfs");
   }
 }
 

Shares/SMB.pm
@@ -317,7 +317,7 @@ sub set_smb_users
     {
       # User deleted
       $user = $old_hash{$i};
-      &Utils::File::run ("pdbedit --delete -u $$user[0]");
+      &Utils::File::run ("pdbedit", "--delete", "-u", $$user[0]);
     }
   }
 }

Time/NTP.pm
@@ -158,7 +158,7 @@ sub apply_ntp_date
 
   # run ntpdate, this will only be effective
   # when there isn't any NTP server running
-  &Utils::File::run ("ntpdate -b $servers") if ($servers);
+  &Utils::File::run ("ntpdate", "-b", $servers) if ($servers);
 }
 
 sub get

Time/TimeDate.pm
@@ -43,7 +43,7 @@ sub get_utc_time
 sub change_timedate
 {
   my ($time) = @_;
-  my ($command);
+  my (@command);
 
   my $system_table = {
     "Linux"   => "date -u %02d%02d%02d%02d%04d.%02d",
@@ -51,13 +51,13 @@ sub change_timedate
     "SunOS"   => "date -u %02d%02d%02d%02d%04d.%02d",
   };
 
-  $command = sprintf ($$system_table {$Utils::Backend::tool{"system"}},
-                      $$time{"month"} + 1, $$time{"monthday"},
-                      $$time{"hour"},  $$time{"minute"}, 
-                      $$time{"year"},  $$time{"second"});
+  @command = ($$system_table {$Utils::Backend::tool{"system"}},
+              $$time{"month"} + 1, $$time{"monthday"},
+              $$time{"hour"}, $$time{"minute"},
+              $$time{"year"}, $$time{"second"});
 
-  &Utils::Report::do_report ("time_localtime_set", $command);
-  return &Utils::File::run ($command);
+  &Utils::Report::do_report ("time_localtime_set", @command);
+  return &Utils::File::run (@command);
 }
 
 sub set_utc_time
@@ -73,7 +73,7 @@ sub set_utc_time
 
 sub time_sync_hw_from_sys
 {
-  &Utils::File::run ("hwclock --systohc") if ($$tool{"system"} eq "Linux");
+  &Utils::File::run ("hwclock", "--systohc") if ($$tool{"system"} eq "Linux");
   return 0;
 }
 
@@ -116,7 +116,7 @@ sub get_timezone
     $size_test = (stat("$zoneinfo_dir/$zone"))[7];
     if ($size_test eq $size_search)
     {
-      if (!&Utils::File::run ("diff $zoneinfo_dir/$zone $local_time_file"))
+      if (!&Utils::File::run ("diff", "$zoneinfo_dir/$zone", $local_time_file))
       {
         # Found a match.
         last;

commit e2a6668dcd5b4e85c0a5f7ea53198776617d2ef9

Fix user password modification
Commit ee59351bfa31ee60779a20a79b1b49f308a777b0 broke it by removing the password parameter.

Users/Users.pm
@@ -674,9 +674,11 @@ sub change_user
     @command = ($cmd_usermod, "-d", $$new_user[$HOME],
                               "-g", $$new_user[$GID],
                               "-l", $$new_user[$LOGIN],
+                              "-p", $$new_user[$PASSWD],
                               "-s", $$new_user[$SHELL],
                               "-u", $$new_user[$UID],
                                     $$old_user[$LOGIN]);
+
     &Utils::File::run (@command);
   }
 